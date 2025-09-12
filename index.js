e32b79e4692794e2b47a282644938d865b1535f4
Linda <jeropkoimur@gmail.com>
2025-09-12 11:11:36 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride <= 400) {
+    result = "This one is on me!"
+  }
+  else if (ride > 400 && ride < 2000) {
+    result = "That will be twenty bucks."
+  }
+  if (ride > 2000 && ride <= 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500){
+    result = "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination
+  if (city === 'NYC'){
+    destination = 'Ok, sounds good.'
+  }
+
+  else {
+    destination = 'No go.'
+  }
+  return destination
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip){
+    case 'generous':
+    response = "Thank you so much.";
+    break;
+
+    case 'not as generous':
+    response = "Thank you.";
+    break;
+
+    default:
+    response = "Bye.";
+  }
+  return response;
 }
